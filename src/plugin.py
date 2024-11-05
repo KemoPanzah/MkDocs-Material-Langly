0069dd97fbfd6db8799412c317725d63c02f7ae4
Kemo <kemo@PROTON-PC-01>
2024-11-05 18:11:11 +0100

    +

--- src/plugin.py
+++ src/plugin.py
@@ -32,6 +32,7 @@ class Langly(BasePlugin):
         ('targets', config_options.Type(list, default=[])),
         # ('delimiter', config_options.Type(str, default='[[,]]')),
         ('lang_switch', config_options.Type(bool, default=True)),
+        ('mode', config_options.Type(str, default='manual')),
     )
 
     def __init__(self):
