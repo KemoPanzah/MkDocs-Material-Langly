a4373e75be1c6bc94175b45830bc72a33e259b7f
Kemo <kemo@PROTON-PC-01>
2025-03-09 11:52:38 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mkdocs-material-langly',
-    version='0.1.2',
+    version='0.1.3',
     description='A language tool for MkDocs Material theme',
     long_description='A language tool for MkDocs Material theme',
     keywords='mkdocs, material, language, translation, multilanguage, multilingual, deepl',
